e2e0b7315082169197142e6aad93092fafb05ac3
carloslevo <madrilevo16@gmail.com>
2021-05-21 10:29:04 +0200

    Task added to remote repoo

--- src/test/java/com/blinkLearn/runners/FailedTestRunner.java
+++ src/test/java/com/blinkLearn/runners/FailedTestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"html:target/fail_html_report"},
         features = "@target/rerun.txt",
-        glue = "com/manomano/step_definitions"
+        glue = "com/blinkLearn/step_definitions"
 
 )
 
